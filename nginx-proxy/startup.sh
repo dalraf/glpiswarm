65449971ab9b6800e4ebc89398dc432d6be4d008
Daniel Fonseca Alves <dalraf@gmail.com>
2017-02-03 16:43:38 -0200

    Correção

--- nginx-proxy/startup.sh
+++ nginx-proxy/startup.sh
@@ -3,7 +3,7 @@ nginx -g "daemon off;" &
 pidof nginx
 
 
-if ! [ -d /etc/letsencrypt/live/$dominioservice ] || ! $sll>/dev/null 2>&1
+if ! [ -d /etc/letsencrypt/live/$dominioservice ] || ! $ssl >/dev/null 2>&1
 then
 
     ! [ -d /usr/share/nginx/html/$dominioservice ] && mkdir /usr/share/nginx/html/$dominioservice
@@ -16,7 +16,7 @@ then
 
 fi
 echo "Criando aquivo de final do ngnix"
-if [[ $ssl ]]
+if $ssl
 then
     sed  "s/addressservice/$addressservice/g;s/portservice/$portservice/g;s/dominioservice/$dominioservice/g" /default.end > /etc/nginx/conf.d/default.conf 
 else
@@ -26,7 +26,7 @@ cat /etc/nginx/conf.d/default.conf
 nginx -s reload
 while true;
 do
-    if [[ $ssl ]]
+    if $ssl 
     then  
         echo Recriando certificado
         certbot renew --renew-hook "nginx -s reload"
