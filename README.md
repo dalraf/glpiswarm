b7fdbc22d3c4f828eccf534c1ca460e31811c53a
Arcom <arcom@arcominf.com.br>
2017-02-03 15:43:27 -0200

    Adicionado arquivos

---
+++ README.md
@@ -0,0 +1,1 @@
+GLPI via docker composer
---
+++ nginx-proxy/startup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+nginx -g "daemon off;" &
+pidof nginx
+
+
+if ! [ -f /etc/nginx/sites-enabled/default ] >/dev/null 2>&1
+then
+
+    ! [ -d /usr/share/nginx/html/$dominioservice ] && mkdir /usr/share/nginx/html/$dominioservice
+    ! [ -d /usr/share/nginx/html/$dominioservice/.well-known ] && mkdir /usr/share/nginx/html/$dominioservice/.well-known
+    echo "Criando aquivo de inicial do ngnix"
+    sed  "s/addressservice/$addressservice/g;s/portservice/$portservice/g;s/dominioservice/$dominioservice/g" /default.init > /etc/nginx/conf.d/default.conf 
+    cat /etc/nginx/conf.d/default.conf
+    nginx -s reload
+    certbot certonly -n --webroot --agree-tos --email $emailregister -w /usr/share/nginx/html/$dominioservice -d $dominioservice -d $dominioservice
+    echo "Criando aquivo de final do ngnix"
+    sed  "s/addressservice/$addressservice/g;s/portservice/$portservice/g;s/dominioservice/$dominioservice/g" /default.end > /etc/nginx/conf.d/default.conf 
+    cat /etc/nginx/conf.d/default.conf
+    nginx -s reload
+fi
+
+while true;
+do
+    echo Recriando certificado
+    certbot renew --renew-hook "nginx -s reload"
+    sleep 10
+done
